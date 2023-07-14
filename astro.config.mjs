8a9fa3f00d7c4568f4a9c656c94324b686119102
yaKsirhC <chrisuser199@gmail.com>
2023-07-14 22:58:36 +0300

    Fix SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), tailwind(), svelte()],
-  output: 'server'
+  output: 'server',
+  adapter: vercel(),
 });
