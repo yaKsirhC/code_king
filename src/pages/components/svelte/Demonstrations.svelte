6e4f85b66cd509777df705d3636de7d5504a6950
yaKsirhC <chrisuser199@gmail.com>
2023-07-15 11:33:18 +0300

    Fix Demos

--- src/pages/components/svelte/Demonstrations.svelte
+++ src/pages/components/svelte/Demonstrations.svelte
@@ -4,43 +4,57 @@
 	let cardIndex = 0;
 </script>
 
-<section id="demos" class="demo flex justify-evenly xl:items-start items-center xl:flex-row xl: flex-col-reverse xl:gap-16 gap-8">
-	<div class="description">
-		<div class="genres-cole grid place-content-center">
-			<p class="xl:text-start sm:text-center max-w-none text-start px-4 text-gray-700">
+<section
+	id="demos"
+	class="demo flex justify-evenly xl:items-start items-center xl:flex-row xl: flex-col-reverse xl:gap-16 gap-8"
+>
+	<div class="description grid">
+		<div class="flex flex-col gap-4 flex-grow">
+			<p
+				class="xl:text-start sm:text-center max-w-none px-4 text-gray-700"
+			>
 				{demos.at(cardIndex)?.genres}
 			</p>
-		</div>
-		<div class="flex flex-col gap-4 flex-grow">
 			<p class="sm:text-3xl text-lg px-4 xl:text-start text-center">
 				{demos.at(cardIndex)?.paragraph}
 			</p>
-			<div class="btns my-auto p-t16 xl:self-end self-center flex gap-8">
-				<button class="text-blue-500 sm:text-3xl text-lg hover:bg-blue-50 px-4"
-					on:click={() => {
-						cardIndex = (cardIndex - 1) % 4;
-					}}>&larr;</button
-				>
-				<button class="text-blue-500 sm:text-3xl text-lg hover:bg-blue-50 px-4 py-2"
-					on:click={() => {
-						cardIndex = (cardIndex + 1) % 4;
-					}}>&rarr;</button
-				>
-			</div>
+		</div>
+		<div class="btns my-auto p-t16 xl:justify-self-end justify-self-center flex gap-8">
+			<button
+				class="text-blue-500 sm:text-3xl text-lg hover:bg-blue-50 px-4"
+				on:click={() => {
+					cardIndex = (cardIndex - 1) % 4;
+				}}>&larr;</button
+			>
+			<button
+				class="text-blue-500 sm:text-3xl text-lg hover:bg-blue-50 px-4 py-2"
+				on:click={() => {
+					cardIndex = (cardIndex + 1) % 4;
+				}}>&rarr;</button
+			>
 		</div>
 	</div>
-	<a target="_blank" href={demos.at(cardIndex)?.credit} class="cool flex flex-col items-center">
+	<a
+		target="_blank"
+		href={demos.at(cardIndex)?.credit}
+		class="cool flex flex-col items-center"
+	>
 		<img
 			src={demos.at(cardIndex)?.media}
 			alt={demos.at(cardIndex)?.media}
 		/>
-		<p class="text-gray-700 sm:text-xl text-xs mt-2 px-4 text-center sm:text-start">
+		<p
+			class="text-gray-700 sm:text-xl text-xs mt-2 px-4 text-center sm:text-start"
+		>
 			{demos.at(cardIndex)?.credit}
 		</p>
 	</a>
 </section>
 
 <style lang="css">
+	.description {
+		grid-template-rows: 400px max-content;
+	}
 	img {
 		object-fit: contain;
 		/* height: 400px; */
